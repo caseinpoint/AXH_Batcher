2bc62be93abf7deb663f481afcdab2d71301b817
Drue Gilbert <drue.gilbert@gmail.com>
2021-03-12 16:10:59 -0800

    finished options page

--- scripts/options.js
+++ scripts/options.js
@@ -1,3 +1,21 @@
 $(document).ready(() => {
-	console.log("document ready")
+	// populate current values:
+	chrome.storage.sync.get(['pickup_street', 'pickup_street2', 'pickup_city', 'pickup_state', 'pickup_zipcode', 'storeNumber', 'storePhone'], (result) => {
+		for (let key in result) {
+			$('#'+key).val(result[key]);
+		}
+	});
+
+	// handle submit and save values
+	$('#options_form').submit((event) => {
+		event.preventDefault();
+		let options = {};
+		const inputs = $('#options_form').serializeArray();
+		for (let input of inputs) {
+			options[input['name']] = input['value'];
+		}
+		chrome.storage.sync.set(options, () => {
+			$('#success').removeClass('visually-hidden');
+		});
+	});
 });
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,24 @@
 $(document).ready(() => {
-	console.log("document ready")
+	// get pickup info from settings
+	var storeInfo = {};
+	chrome.storage.sync.get(['pickup_street', 'pickup_street2', 'pickup_city', 'pickup_state', 'pickup_zipcode', 'storeNumber', 'storePhone'], (result) => {
+		console.log(result);
+		storeInfo = result;
+	});
+
+	// scrape invoice and return dropoff info
+	function getDropoffInfo() {
+
+	}
+
+	// build csv and return encoded URI
+	function getCSV(dropoffInfo, numBags, numBoxes) {
+		const csvColumns = 'pickup_street,pickup_street2,pickup_city,pickup_state,pickup_zipcode,order_id,dropoff_street,dropoff_street2,dropoff_city,dropoff_state,dropoff_zipcode,dropoff_earliest_datetime,dropoff_latest_datetime,dropoff_buffer,customer_name,customer_email,customer_phone_number,pickup_notes,dropoff_notes,delivery_items,signature_required,delivery_proof_photo_required,sms_enabled,dimension_unit,height,width,length,weight_unit,weight';
+
+	}
+
+	// handle batch_form submit
+	$('#batch_form').submit((event) => {
+		event.preventDefault();
+	});
 });
