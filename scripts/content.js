64730ae028015841939a3c86e3af698b9a07e4c5
Drue Gilbert <drue.gilbert@gmail.com>
2021-03-23 12:00:33 -0700

    progress on scrapeInvoice()

--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,31 @@
 $(document).ready(() => {
 	console.log('"content.js" injected');
 
-	const csvColumns = 'pickup_street,pickup_street2,pickup_city,pickup_state,pickup_zipcode,order_id,dropoff_street,dropoff_street2,dropoff_city,dropoff_state,dropoff_zipcode,dropoff_earliest_datetime,dropoff_latest_datetime,dropoff_buffer,customer_name,customer_email,customer_phone_number,pickup_notes,dropoff_notes,delivery_items,signature_required,delivery_proof_photo_required,sms_enabled,dimension_unit,height,width,length,weight_unit,weight'.split(',');
+	const REGEX_EMAIL = /\S+@\S+\.\S+/;
+	const REGEX_PHONE = /(\d{3}-\d{3}-\d{4})|(\d{10})/g;
 
-	// scrape and return customer name/phone/email/address:
-	function scrapeDropoffInfo() {
-		var dropoffInfo = {};
+	const CSV_COLUMNS = 'pickup_street,pickup_street2,pickup_city,pickup_state,pickup_zipcode,order_id,dropoff_street,dropoff_street2,dropoff_city,dropoff_state,dropoff_zipcode,dropoff_earliest_datetime,dropoff_latest_datetime,dropoff_buffer,customer_name,customer_email,customer_phone_number,pickup_notes,dropoff_notes,delivery_items,signature_required,delivery_proof_photo_required,sms_enabled,dimension_unit,height,width,length,weight_unit,weight'.split(',');
 
-		const emailRegex = /\S+@\S+\.\S+/;
-		dropoffInfo['customer_email'] = $('.BillingAddress').text().match(emailRegex)[0];
+	// scrape and return info needed from invoice:
+	function scrapeInvoice(storePhone) {
+		var invoiceInfo = {};
 
-		dropoffInfo['customer_name'] = $('.ShippingAddress').children()[1].innerText;
+		invoiceInfo['customer_name'] = $('.ShippingAddress').children()[1].innerText;
 
-		return dropoffInfo;
+		invoiceInfo['customer_email'] = $('.BillingAddress').text().match(REGEX_EMAIL)[0];
+
+		let phoneMatch = $('.ShippingAddress').text().match(REGEX_PHONE);
+		if (phoneMatch !== null) {
+			// last match will be mobile number if present:
+			invoiceInfo['customer_phone_number'] = phoneMatch[phoneMatch.length - 1];
+		} else {
+			// no match means no valid phone numbers, so use store's:
+			invoiceInfo['customer_phone_number'] = storePhone;
+		}
+
+		// TODO: dropoff_notes
+
+		return invoiceInfo;
 	}
 
 	// listen for message from extension:
@@ -20,7 +33,7 @@ $(document).ready(() => {
 		sendResponse('"content.js" recieved data');
 		console.log(msg);
 
-		var dropoffInfo = scrapeDropoffInfo();
-
+		var invoiceInfo = scrapeInvoice(msg['storeInfo']['storePhone']);
+		console.log(invoiceInfo);
 	});
 });
